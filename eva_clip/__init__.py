a64b1fc767fbae5f1f36a4708ce1a87d649804a4
Yanze Wu <yanze@users.noreply.huggingface.co>
2024-05-01 15:42:42 +0000

    Update eva_clip/__init__.py

--- eva_clip/__init__.py
+++ eva_clip/__init__.py
@@ -1,7 +1,6 @@
 from .constants import OPENAI_DATASET_MEAN, OPENAI_DATASET_STD
 from .factory import create_model, create_model_and_transforms, create_model_from_pretrained, get_tokenizer, create_transforms
 from .factory import list_models, add_model_config, get_model_config, load_checkpoint
-from .loss import ClipLoss
 from .model import CLIP, CustomCLIP, CLIPTextCfg, CLIPVisionCfg,\
     convert_weights_to_lp, convert_weights_to_fp16, trace_model, get_cast_dtype
 from .openai import load_openai_model, list_openai_models
