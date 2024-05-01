a0b66e1d011d501ca5c6c1c1344e632ecc142b9c
Yanze Wu <yanze@users.noreply.huggingface.co>
2024-05-01 16:45:21 +0000

    Update pulid/pipeline.py

--- pulid/pipeline.py
+++ pulid/pipeline.py
@@ -217,6 +217,7 @@ class PuLIDPipeline:
         return torch.cat((uncond_id_embedding, id_embedding), dim=0)
 
     def inference(self, prompt, size, prompt_n='', image_embedding=None, id_scale=1.0, guidance_scale=1.2, steps=4):
+        print(self.pipe.unet.time_embedding.linear_1.weight.data.dtype)
         images = self.pipe(
             prompt=prompt,
             negative_prompt=prompt_n,
